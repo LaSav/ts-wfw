c8c9be3537f0adc93312642d30a5f3f59c67986d
savva <savva.lambin@gmail.com>
2023-07-24 14:39:18 +0100

    Add User Class with get, set, on, trigger

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log('hello with vite');
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+
+user.on('change', () => {
+  console.log('change 1');
+});
+user.on('change', () => {
+  console.log('change 2');
+});
+user.on('save', () => {
+  console.log('save was triggered');
+});
+
+user.trigger('save');
